fa7ac4280f42632619809ba1d69f3b3791a9065b
stoke <stoke95@gmail.com>
2012-11-03 21:01:54 +0100

    resize

--- init.lua
+++ init.lua
@@ -17,19 +17,37 @@ function move(direction, step)
   focused:geometry(geo)
 end
 
+function resize(direction, step)
+  local focused = client.focus
+  local geo = focused:geometry()
+
+  if     direction == "Up"    then geo['height'] = geo['height'] - step
+  elseif direction == "Down"  then geo['height'] = geo['height'] + step
+  elseif direction == "Left"  then geo['width'] = geo['width'] - step
+  elseif direction == "Right" then geo['width'] = geo['width'] + step
+  end
+
+  focused:geometry(geo)
+end
+
 function table(step)
-  local modifiers = { "Mod4", "Control" }
+  local mmodifiers = { "Mod4", "Control" }
+  local rmodifiers = { "Mod4", "Mod1" }
   local directions = { "Up", "Down", "Right", "Left" }
   local keys = {}
   
   for k, v in ipairs(directions) do
     keys = awful.util.table.join(keys,
-      awful.key(modifiers, v, function ()
-        move(v, step or 3)
+      awful.key(mmodifiers, v, function ()
+        move(v, step or 18)
+      end),
+
+      awful.key(rmodifiers, v, function ()
+        resize(v, step or 18)
       end)
     )
   end
-
+  
   return keys
 end
 
